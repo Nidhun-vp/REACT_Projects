f579a753e29da1d84a734b3f34e3ab1b8c244257
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2024-10-19 12:22:05 +0530

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Main from './Main';
-function Header()
-{
-    return(
-        <div>Hello world
-            <p>
-                <Main/>
-            </p>
-        </div>
-    
-    )
+import React from 'react'
+import"../styles/Header.css"
+
+function Header() {
+  return (
+    <div>
+      <h1 className='p1'>this content loaded from header</h1>
+      <p className='p1'>hello good morning</p>
+      <p className='p1'>Demo text</p>
+    </div>
+  )
 }
+
 export default Header
