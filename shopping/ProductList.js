9c7f6a5f331864b75f8523bc9f98c395d8228cdd
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2024-10-23 12:07:20 +0530

    Add files via upload

---
+++ shopping/ProductList.js
@@ -0,0 +1,41 @@
+const products=[
+    {
+    id:1,
+    name:"bread",
+    description:"description for teddy",
+    imageUrl:"bread.jpg",
+    },
+    {
+        id:2,
+        name:"Nestle",
+        description:"description for Nestle",
+        imageUrl:"nestle.png",
+        },
+    {
+        id:3,
+        name:"vegetables",
+        description:"Description for Doll",
+        imageUrl:"Vegetables.jpg",
+    },
+    {
+        id:4,
+        name:"Grocery",
+        description:"Description for vegetables",
+        imageUrl:"Vegetables.jpg",
+    },
+    {
+        id:5,
+        name:"Fruits",
+        description:"Description for Fruits",
+        imageUrl:"fruits.jpg",
+    },
+    {
+        id:6,
+        name:"Bakery",
+        description:"Description for Bakery",
+        imageUrl:"bakery.jpeg",
+    },
+
+
+];
+export default products
